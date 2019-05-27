246275577ff37ac7c13c323cf83580c2a0f6945d
chuxmykel <ngwobiachukwudi@gmail.com>
2019-05-27 18:13:45 +0100

    chore(server setup): initialize express server with environment variables
    - install dotenv and update server to run on port gotten from env variables
    - add dev and start scripts for development and production environments
    - create server folder and move all server related filed to the folder
    - update README.md file with instructions on how to run the project
    
    [Finishes #166265828]

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('This is Hello Books'));
 
