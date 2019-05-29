9f278849b43fe303a6f4800ab0f99fc32b0da5a8
chuxmykel <ngwobiachukwudi@gmail.com>
2019-05-29 11:57:42 +0100

    chore(database): configure postgresql with sequelize
    - install sequelize as ORM and sequelize-cli for operations
    - add users migrations and models for table creation and db interaction
    - update .env.sample file to contain database url
    - add database related scripts
    - update README.md file to describe new database scripts
    
    [Delivers #166265830]

---
+++ server/app/config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports =  {
+  "development": {
+    "use_env_variable": "DATABASE_URL"
+  },
+  "test": {
+    "use_env_variable": "TEST_DATABASE_URL"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  }
+};
