2ae7484dad2fd58df110caae7835e400fa26d362
ovieMudi <oviisaacs@gmail.com>
2019-05-30 06:04:53 +0100

    move swagger documention to API v1 route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import swaggerUI from 'swagger-ui-express';
-import yaml from 'yamljs';
 import route from './route/index';
 
 dotenv.config();
@@ -9,10 +7,6 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-const swaggerApiDoc = yaml.load(`${__dirname  }/docs/hellobooks_api_doc.yaml`);
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerApiDoc));
 
 app.use('/api/v1/', route);
 
