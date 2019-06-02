94a98648e8e751747d59313b217be731030a22ea
Samuel Ocran <sammiestt@gmail.com>
2019-06-02 13:02:20 +0100

    feature(sign-in): update email signin

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,10 +6,6 @@ const userRoute = express.Router();
 
 userRoute.post('/signup', userController.signUp);
 
-<<<<<<< HEAD
 userRoute.post('/signin', Validate.signin, userController.emailSignin);
-=======
-userRoute.post('/signin', userController.signIn);
->>>>>>> 9b30c55f11b3294a8025cd2e25d10b8f9c6a31bf
 
 export default userRoute;
