7ede7d08407b1542cf4d43c285d4ac5e9ca5eef4
encodedBicoding <dominicisioma000@gmail.com>
2019-06-02 20:57:47 +0100

    Remove console.log statements

--- server/index.js
+++ server/index.js
@@ -27,7 +27,6 @@ app.all('*', (req, res) =>
 );
 
 if (process.env.NODE_ENV !== 'test') {
-  //app.listen(port, () => log(`Server is running on PORT ${port}`));
-  app.listen(port, console.log(`App running on PORT ${port}`))
+  app.listen(port, () => log(`Server is running on PORT ${port}`));
 }
 export default app;
