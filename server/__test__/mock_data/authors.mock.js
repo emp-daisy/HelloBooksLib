5a25701631f69a9828802fbef1855576eb276cc5
Samuel Ocran <sammiestt@gmail.com>
2019-06-04 22:05:18 +0100

    feature(authors-endpoint): implement the ability to add authors
    Add /authors endpoint to router
    Add validation middleware for user inputs
    Attach addAuthor controller to /authors endpoint as route handler
    Get author firstName, lastName from req.body
    Insert author into authors table
    Return response to user
    [Delivers #166265840]

---
+++ server/__test__/mock_data/authors.mock.js
@@ -0,0 +1,18 @@
+const author = {
+  validInputs: {
+    firstName: 'Kelvin',
+    lastName: 'Rockson'
+  },
+
+  incompleteData: {
+    firstName: '',
+    lastName: ''
+  },
+
+  wrongInputType: {
+    firstName: 'John John',
+    lastName: '---???'
+  },
+}
+
+export default author;
---
+++ server/controllers/authorController.js
@@ -0,0 +1,25 @@
+/* eslint-disable require-jsdoc */
+import models from '../db/models';
+import util from '../helpers/utilities';
+
+class AuthorController {
+  static async addAuthor(req, res) {
+    const { firstName, lastName } = req.body;
+    try {
+      const author = await models.Authors.create({
+        firstName,
+        lastName
+      });
+
+      return util.successStatus(res, 201, 'Author added successfully', {
+        authorId: author.id,
+        firstName: author.firstName,
+        lastName: author.lastName
+      });
+    } catch (error) {
+      util.errorStatus(res, 500, error.name);
+    }
+  }
+}
+
+export default AuthorController;
