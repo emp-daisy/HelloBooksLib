e527b0202dac110475ef18868c6c7470e793b56e
ovieMudi <oviisaacs@gmail.com>
2019-06-06 17:44:55 +0100

    feat(book-listing): add route route for listing books
    - add new GET route for listing books
    - configure controller method for listing and paginating books
    - generate Book objects using faker package and seed Books table
    - write tests for book endpoints
    
    [#166265864]

--- server/__test__/book.spec.js
+++ server/__test__/book.spec.js
@@ -5,10 +5,9 @@ import mockBook from './mock_data/mock_books';
 const server = () => supertest(app);
 const url = '/api/v1';
 
-
 describe('Books tests', () => {
   describe('test for add books', () => {
-    it('Should add a new book when all required input is supplied', async (done) => {
+    it('Should add a new book when all required input is supplied', async done => {
       server()
         .post(`${url}/books`)
         .send(mockBook.completeBookData)
@@ -25,30 +24,104 @@ describe('Books tests', () => {
           expect(Object.keys(res.body.data)).toMatchSnapshot();
         });
     });
-    it('Should throw an error when inputs are not supplied', async (done) => {
+    it('Should throw an error when inputs are not supplied', async done => {
       server()
         .post(`${url}/books`)
         .send(mockBook.emptyBookData)
         .end((err, res) => {
           expect(res.statusCode).toEqual(400);
           expect(res.body.status).toEqual(400);
-          expect(res.body.error[0]).toEqual('Title can not be left empty: Please input title');
-          expect(res.body.error[1]).toEqual('Description can not be left empty: Please input description');
-          expect(res.body.error[2]).toEqual('Amount can not be left empty: Please input amount');
-          expect(res.body.error[3]).toEqual('AuthorID can not be left empty: Please input amount');
+          expect(res.body.error[0]).toEqual(
+            'Title can not be left empty: Please input title'
+          );
+          expect(res.body.error[1]).toEqual(
+            'Description can not be left empty: Please input description'
+          );
+          expect(res.body.error[2]).toEqual(
+            'Amount can not be left empty: Please input amount'
+          );
+          expect(res.body.error[3]).toEqual(
+            'AuthorID can not be left empty: Please input amount'
+          );
           done();
         });
     });
-    it('Should throw an error when inputs are not supported', async (done) => {
+    it('Should throw an error when inputs are not supported', async done => {
       server()
         .post(`${url}/books`)
         .send(mockBook.unsupportedBookData)
         .end((err, res) => {
           expect(res.statusCode).toEqual(400);
           expect(res.body.status).toEqual(400);
-          expect(res.body.error[0]).toEqual('Amount is not valid currency: Please input a valid amount');
-          expect(res.body.error[1]).toEqual('AuthorID can not be left empty: Please input amount');
-          expect(res.body.error[2]).toEqual('Year is not valid year: Please input a valid year');
+          expect(res.body.error[0]).toEqual(
+            'Amount is not valid currency: Please input a valid amount'
+          );
+          expect(res.body.error[1]).toEqual(
+            'AuthorID can not be left empty: Please input amount'
+          );
+          expect(res.body.error[2]).toEqual(
+            'Year is not valid year: Please input a valid year'
+          );
+          done();
+        });
+    });
+  });
+});
+
+describe('GET Books tests', () => {
+  describe('Tests for listing all books', () => {
+    it('should get all books in the database', done => {
+      server()
+        .get(`${url}/books`)
+        .end((err, res) => {
+          const { data } = res.body;
+          const response = data[0];
+          expect(res.statusCode).toEqual(200);
+          expect(res.body.message).toEqual('success');
+          expect(res.body.status).toEqual(200);
+          expect(response).toHaveProperty('year');
+          expect(response).toHaveProperty('title');
+          expect(response).toHaveProperty('amount');
+          expect(response).toHaveProperty('status');
+          expect(response).toHaveProperty('description');
+          done();
+        });
+    });
+
+    it('should paginate book listing', done => {
+      const page = 1;
+      const limit = 10;
+      server()
+        .get(`${url}/books?page=${page}&limit=${limit}`)
+        .end((err, res) => {
+          const { data } = res.body;
+          const response = data[0];
+          expect(res.statusCode).toEqual(200);
+          expect(res.body).toHaveProperty('pages');
+          expect(response).toHaveProperty('year');
+          expect(response).toHaveProperty('title');
+          expect(response).toHaveProperty('amount');
+          expect(response).toHaveProperty('status');
+          expect(response).toHaveProperty('description');
+          done();
+        });
+    });
+
+    it('should use fallback for paginating listing with invalid page number', done => {
+      const page = 'page';
+      const limit = 'limit';
+      server()
+        .get(`${url}/books?page=${page}&limit=${limit}`)
+        .end((err, res) => {
+          const { data } = res.body;
+          const response = data[0];
+          expect(res.statusCode).toEqual(200);
+          expect(res.body).toHaveProperty('pages');
+          expect(response).toHaveProperty('year');
+          expect(response).toHaveProperty('title');
+          expect(response).toHaveProperty('amount');
+          expect(response).toHaveProperty('status');
+          expect(response).toHaveProperty('description');
           done();
         });
     });
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import debug from 'debug';
 import morgan from 'morgan';
+import faker from 'faker';
 import routes from './routes/index';
 
 config();
@@ -21,6 +22,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.status(301).redirect('/api/v1'));
+
+app.get('/api/faker', (req, res) => {
+  const no = 100;
+  let index;
+  const fakes = [];
+  for (index = 0; index < no; index += 1) {
+    fakes.push({
+      title: faker.lorem.words(),
+      author: faker.name.findName(),
+      author_image: faker.image.avatar(),
+      release_date: faker.date.recent(),
+      image: faker.image.abstract(),
+      price: faker.commerce.price(),
+      short_description: faker.lorem.sentence(),
+      long_description: faker.lorem.paragraph()
+    });
+  }
+  res.json(fakes);
+});
 
 app.use('/api/v1', routes);
 
