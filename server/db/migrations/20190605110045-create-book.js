aac61c65591756dd9290507d1972601723f83c3f
Pelumi Aleshinloye <aleshinloyelukmony@gmail.com>
2019-06-06 07:21:46 +0100

    Update 20190605110045-create-book.js

--- server/db/migrations/20190605110045-create-book.js
+++ server/db/migrations/20190605110045-create-book.js
@@ -21,11 +21,7 @@ export function up(queryInterface, Sequelize) {
       type: Sequelize.NUMERIC
     },
     authorID: {
-<<<<<<< HEAD
       allowNull: true,
-=======
-      allowNull: false,
->>>>>>> feat(addBook): user can add books
       type: Sequelize.INTEGER
     },
     reviewID: {
