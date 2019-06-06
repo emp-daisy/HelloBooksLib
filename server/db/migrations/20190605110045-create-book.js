af378ba66b73e68f47afbbedcc3b3afb06be978e
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-06-06 08:40:07 +0100

    rebase

---
+++ server/db/migrations/20190605110045-create-book.js
@@ -0,0 +1,52 @@
+/* eslint-disable require-jsdoc */
+
+export function up(queryInterface, Sequelize) {
+  return queryInterface.createTable('Books', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      allowNull: false,
+      type: Sequelize.TEXT
+    },
+    description: {
+      allowNull: false,
+      type: Sequelize.TEXT
+    },
+    amount: {
+      allowNull: false,
+      type: Sequelize.NUMERIC
+    },
+    authorID: {
+      allowNull: true,
+      type: Sequelize.INTEGER
+    },
+    reviewID: {
+      allowNull: true,
+      type: Sequelize.INTEGER
+    },
+    status: {
+      allowNull: true,
+      type: Sequelize.BOOLEAN
+    },
+    year: {
+      allowNull: false,
+      type: Sequelize.INTEGER
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+
+export function down(queryInterface) {
+  return queryInterface.dropTable('Books');
+}
